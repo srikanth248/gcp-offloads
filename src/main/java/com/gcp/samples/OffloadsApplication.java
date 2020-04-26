e0d95dbc23af4d881cab8907615bad231008cfe3
Srikanth <srikanth_248@yahoo.com>
2020-04-25 23:42:56 -0400

    Initial Commit

---
+++ src/main/java/com/gcp/samples/OffloadsApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffloadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffloadsApplication.class, args);
+	}
+
+}
